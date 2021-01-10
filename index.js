40dc46e028583ca2e96551271fd32e1f9d034887
BMoClay <benmorgancleveland@gmail.com>
2021-01-10 16:04:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 // Code your solutions in this file
 
 
-function writeCards(array, event) {
-    let thankYouCards = []
-    for (let i = 0; i < array.length; i++) {
-      thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
-    }
+// function writeCards(array, event) {
+//     let thankYouCards = []
+//     for (let i = 0; i < array.length; i++) {
+//       thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+//     }
   
-    return thankYouCards;
-  }
+//     return thankYouCards;
+//   }
 
-function countDown( startingNumber ){
-    while (startingNumber > 0) {
-        console.log( startingNumber );
-        startingNumber -= 1;
-    }
-    console.log( startingNumber );
+// function countDown( startingNumber ){
+//     while (startingNumber > 0) {
+//         console.log( startingNumber );
+//         startingNumber -= 1;
+//     }
+//     console.log( startingNumber );
+// }
+
+function writeCards(namesArray, eventName) {
+  let thankYouCards = []
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return thankYouCards
+}
+
+function countDown(startingNumber) {
+  while (startingNumber > 0) {
+    console.log(startingNumber)
+    startingNumber -= 1
+  }
+  console.log(startingNumber)
 }
